7e6926a7a9c82b55cf4f88a8aadc3e02bd6eb286
Lars Lønne <lars.lonne@kantega.no>
2024-01-31 22:18:37 +0100

    render qr code as <img>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,9 @@
 	import QRCode from 'qrcode';
 
 	let text = 'https://www.kantega.no/';
-	$: promise = QRCode.toString(text, { type: 'svg' });
+	$: promise = QRCode.toString(text, { type: 'svg' }).then((svgString) =>
+		URL.createObjectURL(new Blob([svgString], { type: 'image/svg+xml' }))
+	);
 </script>
 
 <svelte:head>
@@ -14,7 +16,11 @@
 	<h1>Lag QR-kode av en URL</h1>
 	<label for="input">Skriv inn URL:</label>
 	<input type="text" id="input" bind:value={text} />
-	<div id="svg">{#await promise then svg}{@html svg}{/await}</div>
+	<div id="svg">
+		{#await promise then dataURL}
+			<img src={dataURL} alt={`En QR-kode av teksten "${text}"`} />
+		{/await}
+	</div>
 </section>
 
 <style>
