dfee4fc2add3c6d4d592a2c3e42734093b50496c
Lars Lønne <lars.lonne@kantega.no>
2024-01-31 21:46:45 +0100

    first working version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,31 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import QRCode from 'qrcode';
+
+	let text = 'https://www.kantega.no/';
+	$: promise = QRCode.toString(text, { type: 'svg' });
 </script>
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="QR codes" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<h1>Lag QR-kode av en URL</h1>
+	<label for="input">Skriv inn URL:</label>
+	<input type="text" id="input" bind:value={text} />
+	<div id="svg">{#await promise then svg}{@html svg}{/await}</div>
 </section>
 
 <style>
 	section {
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
+		max-width: 800px;
 	}
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	#svg {
+		width: 600px;
+		height: 600px;
 	}
 </style>
