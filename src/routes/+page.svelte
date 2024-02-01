a166d6c7fa86fd073fb27ea475d248ef50308d89
Lars Lønne <lonnelars@gmail.com>
2024-02-01 14:58:36 +0100

    styling adjustments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import QRCode from 'qrcode';
 
 	let text = 'https://www.kantega.no/';
-	$: promise = QRCode.toString(text, { type: 'svg' }).then((svgString) =>
+	$: promise = QRCode.toString(text, { type: 'svg', margin: 0 }).then((svgString) =>
 		URL.createObjectURL(new Blob([svgString], { type: 'image/svg+xml' }))
 	);
 </script>
@@ -14,7 +14,7 @@
 
 <section>
 	<h1>Lag QR-kode av en URL</h1>
-	<div>
+	<div class="input">
 		<label for="input">Skriv inn URL:</label>
 		<input type="text" id="input" bind:value={text} />
 	</div>
@@ -28,10 +28,29 @@
 <style>
 	section {
 		max-width: 800px;
+		font-family: 'Source Sans Pro', Arial, sans-serif;
+		font-size: 1.5rem;
+
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
 	}
 
-	#svg {
-		width: 600px;
-		height: 600px;
+	h1 {
+		margin: 0;
+	}
+
+	.input {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+
+		& input {
+			font-size: 1.5rem;
+			padding: 1rem .5rem;
+			border-radius: 10px;
+			border: 2px solid
+
+		}
 	}
 </style>
